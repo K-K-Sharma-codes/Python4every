fe8a3c0ad5b03b4ff52660097e57f7a495ae8272
K-K-Sharma-codes <krishnakantsharma0929@gmail.com>
2025-08-05 11:21:03 +0530

    Inheritance

--- class.py
+++ class.py
@@ -1,1 +1,16 @@
-class 
+class PartyAnimal:
+    def __init__(self):
+        self.x = 0
+        
+    def party(self):
+        self.x = self.x + 1
+        print("so far ", self.x)
+        
+an = PartyAnimal()
+an.party()
+an.party()
+an.party()
+print("Type ", type(an))
+print("Dir ", dir(an))
+print("Type ", type(an.x))
+print("Type ", type(an.party))
